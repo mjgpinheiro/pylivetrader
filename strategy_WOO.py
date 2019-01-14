ddd75145fde353954df971b26f7952dc0b4213ef
mjgpinheiro <32980947+mjgpinheiro@users.noreply.github.com>
2019-01-14 21:38:03 +0000

    Update strategy_WOO.py

--- strategy_WOO.py
+++ strategy_WOO.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
+mjp-v1
 This is a temporary script file.
 """
+
+from pipeline_live.data.iex.pricing import USEquityPricing
 from zipline.pipeline import Pipeline, CustomFilter
 from zipline.algorithm import attach_pipeline, pipeline_output
 from zipline.pipeline.filters import QTradableStocksUS
-from zipline.pipeline.factors import AverageDollarVolume, AnnualizedVolatility
+from pipeline_live.data.iex.factors import (AverageDollarVolume, AnnualizedVolatility)
 import numpy as np
 import pandas as pd
 
