de273ac9db55a5f05fd95821940ea59ef786adf8
mjgpinheiro <32980947+mjgpinheiro@users.noreply.github.com>
2019-01-14 22:37:05 +0000

    Update strategy_WOO.py

--- strategy_WOO.py
+++ strategy_WOO.py
@@ -22,10 +22,10 @@ from pylivetrader.api import (
 )
 #from zipline.algorithm import attach_pipeline, pipeline_output
 #from zipline.pipeline.filters import QTradableStocksUS
-#from pipeline_live.data.iex.pricing import USEquityPricing
+from pipeline_live.data.iex.pricing import USEquityPricing
 from pipeline_live.data.iex.fundamentals import IEXCompany, IEXKeyStats
 from pipeline_live.data.iex.factors import (
-    SimpleMovingAverage, AverageDollarVolume, AnnualizedVolatility
+    AverageDollarVolume, AnnualizedVolatility
 )
 #from pipeline_live.data.iex.factors import (AverageDollarVolume, AnnualizedVolatility)
 import numpy as np
