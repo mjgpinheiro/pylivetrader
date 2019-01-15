129e4e50440b599305dba594f5472b7649893a69
mjgpinheiro <32980947+mjgpinheiro@users.noreply.github.com>
2019-01-15 16:02:52 +0000

    Update strategy_WOO.py

--- strategy_WOO.py
+++ strategy_WOO.py
@@ -30,7 +30,7 @@ from pipeline_live.data.iex.factors import (
 )
 #from pylivetrader.pipeline.engine import default_populate_initial_workspace
 #from pylivetrader.api import order_target, record, symbol
-from pylivetrader.finance import commission, slippage
+#from pylivetrader.finance import commission, slippage
 
 #from pipeline_live.data.iex.factors import (AverageDollarVolume, AnnualizedVolatility)
 import numpy as np
@@ -40,7 +40,7 @@ from itertools import cycle
 
 import logbook
 
-log = logbook.Logger('algo')
+log = logbook.Logger('strategy_WOO')
 
 
 def record(*args, **kwargs):
