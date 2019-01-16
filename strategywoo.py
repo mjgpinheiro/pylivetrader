b93fc05323fdc86ca77bf1edf39fde8ee18b5fa9
mjgpinheiro <32980947+mjgpinheiro@users.noreply.github.com>
2019-01-16 17:09:53 +0000

    Update strategywoo.py

--- strategywoo.py
+++ strategywoo.py
@@ -2,8 +2,9 @@
 from zipline.pipeline import (Pipeline)
 from zipline.pipeline.factors import (AverageDollarVolume, AnnualizedVolatility)
 from pylivetrader.api import (attach_pipeline, pipeline_output)
-import numpy as np  # needed for NaN handling
-import math  # ceil and floor are useful for rounding
+import iexfinance as iex
+#import numpy as np  # needed for NaN handling
+#import math  # ceil and floor are useful for rounding
 import pandas as pd
 
 def initialize(context):
